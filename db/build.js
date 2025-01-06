34d627a3a3800da5bc522852ed88883a8c471ddf
d.ryabov <comatose.6666@gmail.com>
2025-01-06 20:11:00 +0300

    update yml

--- db/build.js
+++ db/build.js
@@ -2,10 +2,8 @@ const fs = require('fs')
 const connection = require('./pool')
 
 const sqlQuery = fs.readFileSync('./db/build.sql', 'utf-8')
-const createSqlQuery = fs.readFileSync('./db/create.sql', 'utf-8')
-const dropSqlQuery = fs.readFileSync('./db/drop.sql', 'utf-8')
 
-connection.query(dropSqlQuery, (err) => {
+connection.query(sqlQuery, (err) => {
   if (err) {
     console.error(err)
     return
