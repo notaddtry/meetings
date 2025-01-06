26a9b3fc087add40ee6bb246cec2b09755117766
d.ryabov <comatose.6666@gmail.com>
2025-01-06 16:41:36 +0300

    update yml

--- db/db.config.js
+++ db/db.config.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
 
 module.exports = {
-  user: process.env.DB_USER,
+  user: process.env.POSTGRES_USER,
   host:
     process.env.NODE_ENV === 'prod'
       ? process.env.PROD_HOST_DB
       : process.env.DEV_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
 }
--- db/pool.js
+++ db/pool.js
@@ -2,14 +2,14 @@ const { Pool } = require('pg')
 const { user, password, host, database, port } = require('./db.config.js')
 
 const pool = new Pool({
-  user: process.env.DB_USER,
+  user: process.env.POSTGRES_USER,
   host:
     process.env.NODE_ENV === 'prod'
       ? process.env.PROD_HOST_DB
       : process.env.DEV_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
 })
 
 module.exports = pool
